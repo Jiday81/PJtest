b331092842e08732354f4ef62c07062fccd1de4b
Amauryyyyy <audouard.81amau@gmail.com>
2022-02-09 12:04:41 +0100

    ajout

--- src/TP3/Application.java
+++ src/TP3/Application.java
@@ -1,8 +1,10 @@
 package TP3;
 
 import java.awt.Frame;
+import java.awt.event.WindowEvent;
+import java.awt.event.WindowListener;
 
-public class Application extends Frame {
+public class Application extends Frame implements WindowListener {
 
 	public static void main(String[] args) {
 		new Application();
@@ -16,10 +18,53 @@ public class Application extends Frame {
 		Vue vue = new Vue();
 		modl.addObserver(vue);
 		vue.addActionListener(ctrl);
+		this.addWindowListener(this);
 		
 		this.add(vue);
 		this.pack();
 		this.setVisible(true);
+	}
+
+	@Override
+	public void windowActivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosed(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowClosing(WindowEvent arg0) {
+		System.exit(0);
+		
+	}
+
+	@Override
+	public void windowDeactivated(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowDeiconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowIconified(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void windowOpened(WindowEvent arg0) {
+		// TODO Auto-generated method stub
+		
 	}
 
 }
